6b25430cbf5bb8366ef34efb86009ee898bf8d82
sbsprs <134433305+sbsprs@users.noreply.github.com>
2023-05-24 09:02:41 -0500

    se agrega la interfaz

---
+++ logica.py
@@ -0,0 +1,6 @@
+from tkinter import *
+import pygame
+import os
+import shutil
+from mutagen.mp3 import MP3
+from pydub import AudioSegment
--- mpp.py
+++
@@ -1,6 +0,0 @@
-from tkinter import *
-import pygame
-import os
-import shutil
-from mutagen.mp3 import MP3
-from pydub import AudioSegment
